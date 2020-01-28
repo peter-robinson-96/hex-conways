c9f85fedb12cf092bece38e4ca84163315389130
Peter Robinson <peter.frederick.robinson@gmail.com>
2020-01-28 19:16:59 +1300

    Make server.js react compatible

--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const server = express()
-// const path = require('path')
+const path = require('path')
 
-server.get('/', (req, res) => {
-  res.send('Welcome to Conways HEX game of life!')
-})
+// server.get('/', (req, res) => {
+//   res.send('Welcome to Conways HEX game of life!')
+// })
+
+server.use(express.json())
+server.use(express.static(path.join(__dirname, '../public')))
 
 module.exports = server
